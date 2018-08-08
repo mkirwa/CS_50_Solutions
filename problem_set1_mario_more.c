0f99690b97b90e23a1bb627774247759c9ee0115
mkirwa <mahlonkibiwott@gmail.com>
2018-08-07 23:12:31 -0500

    Add files via upload

---
+++ problem_set1_mario_more.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+
+	int n=4;
+
+	for(int i=0; i<n; i++){
+
+		for(int j=0; j<n; j++){
+
+			if(the last place holder is equal to the value of n){
+
+				printf("#");
+			}
+			else{
+				printf(" ");
+			}
+			
+		}
+	}
+}
