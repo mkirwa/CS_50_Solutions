e36a8bde4aca3b7e52fd5dd63e33f145eb790118
mkirwa <mahlonkibiwott@gmail.com>
2018-08-12 18:03:38 -0400

    Add files via upload

--- problem_set1_mario_more.c
+++ problem_set1_mario_more.c
@@ -2,30 +2,23 @@
 
 int main(void){
 
-//You cannot decrease the length of a string but you can equate the length of the string to a given variable and then
-//countinually decrease that variable
-//
-	int num = get_int("enter the number of start ");
-	int counter=num;
+	int main(void){
+
+	int num = get_int("enter the height ");
+	int array[num];
+	int counter=num-1;
 
 	for(int i=0; i<num; i++){
 
 		for(int j=0; j<num; j++){
 
-			//we need to only print the last element in the for loop 
-			//then print the next last element all the way to the 
-			//first element in the following preceding row 
-			//How? Do we store it in an array? so we can access each place holder?
-			//He taught arrays in that class so we should also be looking at arrays
+			if(array[j]>=counter){
 
-			if(counter>=num){
-
-				printf("#");
+				printf(" # ");
 			}
 			else{
-				printf(" ");
+				printf(" * ");
 			}
-
 		}
 		printf("\n");
 		counter--;
