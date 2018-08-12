937fab7c64e736c70165d5c1c502ba258944564f
mkirwa <mahlonkibiwott@gmail.com>
2018-08-12 19:36:17 -0400

    Add files via upload

---
+++ problem_set1_mario_less.c
@@ -0,0 +1,41 @@
+
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+int main(void){
+
+	int n;
+
+	do{
+		n=get_int("enter the pyramids height: ");
+	}
+	while(n<0 || n>=23);
+
+
+	int counter=n-1;
+
+	for(int j=0; j<n; j++){
+
+		for(int i=0; i<=n ;i++){
+			if(counter<=i)
+			{
+				printf("#");
+			}
+			else{
+			printf(" ");
+			}
+		}
+
+		printf("\n");
+		counter--;
+
+
+	}
+
+
+}
