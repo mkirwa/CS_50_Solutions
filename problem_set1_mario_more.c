4ccbe4bf6c99ef03e78965ec6419bbd31e428796
mkirwa <mahlonkibiwott@gmail.com>
2018-08-11 19:14:56 -0500

    Add files via upload

--- problem_set1_mario_more.c
+++ problem_set1_mario_more.c
@@ -2,24 +2,25 @@
 
 int main(void){
 
-//You cannot decrease the length of a string but you can equate the length of the string to a given variable and then 
+//You cannot decrease the length of a string but you can equate the length of the string to a given variable and then
 //countinually decrease that variable
 //
-	int num = get_int("enter the number of start ")
-	int n=4;
-	int counter=n;
+	int num = get_int("enter the number of start ");
+	int counter=num;
 
-	for(int i=0; i<nun; i++){
+	for(int i=0; i<num; i++){
 
 		for(int j=0; j<num; j++){
 
-			if(counter>=counter){
+			if(counter>=num){
 
 				printf("#");
 			}
 			else{
 				printf(" ");
 			}
+
+			printf("\n");
 
 		}
 		counter--;
