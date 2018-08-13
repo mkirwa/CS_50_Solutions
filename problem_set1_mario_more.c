7b39754412c89a1de0ab9dcf770527ed32b914ca
mkirwa <mahlonkibiwott@gmail.com>
2018-08-12 22:35:59 -0400

    Add files via upload

--- problem_set1_mario_more.c
+++ problem_set1_mario_more.c
@@ -1,28 +1,56 @@
 #include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(void){
 
 	int n=4;
 	int counter=3;
+	int counter1=0;
 
-	for(int j=0; j<n; j++){
+	for (int j=0;j<n;j++){
 
-		for(int i=0; i<n ;i++){
-			if(counter<=i)
-			{
-				printf("#");
-			}
-			else{
-			printf(" ");
-			}
+	for(int i=0;i<n;i++){
+
+		if(i>=counter){
+			printf("#");
+			printf("  ");
+
+
+
+
+						for(int e=0; e<n; e++){
+
+								for(int f=0; f<n; f++){
+
+										if(f<=counter1){
+															printf("#");
+														}
+										else{
+												printf(" ");
+											}
+
+									}
+
+									counter1++;
+									break;
+								}
+
+
+
+
+
+
+
 		}
-
-		printf("\n");
-		counter--;
-
+		else{
+			printf(" ");
+		}
+	}
+	counter--;
+	printf("\n");
 
 	}
-
-
-	
 }
