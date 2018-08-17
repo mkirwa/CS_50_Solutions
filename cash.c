b9623d4d99996998b430db15665cf956ec9890c3
mkirwa <mahlonkibiwott@gmail.com>
2018-08-17 11:52:43 -0400

    Add files via upload

---
+++ cash.c
@@ -0,0 +1,20 @@
+
+
+int main(void){
+    
+    //prompt the user for an amount of change
+        //amount must make sense
+        //what values are accepted
+        //use get_float
+    //validate the input
+        //convert the dollar into cts
+        //multiply it by 100 and also round off
+    //always use the largest coin
+    //keep track of coins used
+        //how many coins to be returned
+        //amount to be returned
+    //print the final number of coins
+    
+    
+    
+}
