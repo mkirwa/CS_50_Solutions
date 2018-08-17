e4598fb25a3285f513e84d67934894c807ae1f7e
mkirwa <mahlonkibiwott@gmail.com>
2018-08-17 16:54:11 -0400

    Add files via upload

--- cash.c
+++ cash.c
@@ -1,21 +1,37 @@
 
 
+
+
 int main(void){
     
     //prompt the user for an amount of change
-         //get the amount in dollars
+        //get the amount in dollars
         //amount must make sense
         //what values are accepted
         //use get_float
     //validate the input
         //convert the dollar into cts
         //multiply it by 100 and also round off
+        
     //always use the largest coin
     //keep track of coins used
         //how many coins to be returned
         //amount to be returned
     //print the final number of coins
     
+    //while(quarters can be used)while the amount is greater than or equal to 25cts
+        //increase count to be returned to the customer
+        //decrease count owned by the user 
+    //while(dimes can be used)
+        //increase count to be returned to the customer
+        //decrease count owned by the user 
+    //while(nickels can be used)
+        //increase count to be returned to the customer
+        //decrease count owned by the user 
+    //while(pennies can be used)
+        //increase count to be returned to the customer
+        //decrease count owned by the user 
     
+
     
 }
