3103f52665b5c3d8a96cae6640d7e45f9332cb67
mkirwa <mahlonkibiwott@gmail.com>
2018-08-19 08:19:30 -0500

    Add files via upload

---
+++ credit.c
@@ -0,0 +1,27 @@
+ #include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    
+    //prompts the user for credit card number
+    //reports whether it's a valide mastercard, visa card or amex
+    //
+    
+    while(the first two numbers are 34 or 35){
+        
+        printf("AMEX\n");
+    }
+    
+    while(the first two numbers are 51 or 52 or 53 or 54 or 55){
+        
+        printf("MASTERCARD\n");
+    }
+    while(the first two numbers starts with 4){
+        
+        printf(" VISA\n");
+    }
+    while(invalid){
+        printf("invalid\n");
+    }
+    
+}
