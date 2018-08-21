89362b0db3c5e1062fbabccb158b784744b73850
mkirwa <mahlonkibiwott@gmail.com>
2018-08-20 23:49:25 -0500

    Add files via upload

--- credit.c
+++ credit.c
@@ -2,11 +2,15 @@
 #include <cs50.h>
 
 int main(void){
+
+
+
     //prompting the user for an input
     long long int number = get_long_long("enter the credit card number: ");
 
     //validate the card
-     //multipy every other digit
+
+    //multipy every other digit
     //sum those digits
     //add to remaining digits
     //validate check sums
@@ -24,15 +28,15 @@ int main(void){
         // n = n/10
 
         printf("%lld\n",number%10);
-
-        number /= 10;
+        
         count++;
-
-
         if(count%2==0){
         product = (number%10)*2;
        printf(" this is the product: %d\n",product);
         }
+
+        number /= 10;
+
     }
 
 
@@ -59,6 +63,7 @@ int main(void){
 
         printf(" VISA\n");
     }
+
 
     //while (number != 0) {
       // printf("%11lld\n",number % 10);
