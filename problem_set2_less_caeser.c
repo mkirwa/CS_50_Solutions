74b2b6b80c59db6fdfd5da889cec4a758116719a
mkirwa <mahlonkibiwott@gmail.com>
2018-08-23 18:19:25 -0500

    Add files via upload

---
+++ problem_set2_less_caeser.c
@@ -0,0 +1,11 @@
+//get the key from the user
+    //in terms of keys.
+    //like a key 2 will change a to c  and z to b
+    //use 2nd command line argument
+    //atoi(new function)
+//get the plain text
+    //use get_string to prompt the user for a text
+//encypher it for them
+    //one character
+    //entire plain text
+//print cyphertext
