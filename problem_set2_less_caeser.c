a40da5fcdbbac63351680facfa7335c093a98681
mkirwa <mahlonkibiwott@gmail.com>
2018-08-23 20:47:59 -0500

    Add files via upload

--- problem_set2_less_caeser.c
+++ problem_set2_less_caeser.c
@@ -1,24 +1,57 @@
 
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
 
-//get the key from the user
-    //in terms of keys.
-    //like a key 2 will change a to c  and z to b
-    //use 2nd command line argument
-    //atoi(new function)
-//get the plain text
-    //use get_string to prompt the user for a text
-//encypher it for them
-    //one character
-    //entire plain text
-//print cyphertext
 
-//pseudocode
-    //get key from command line argument
-    //turn key into integer
-    //prompt for plain text
-    //for each character in the plain text string
+//getting the key from the user using
+//a 2nd command line argument
+int main(int argc, string argv[]){
+
+    if(argc==2){
+
+        string num = argv[1];
+        //atoi(new function) to change string to num
+        int i = atoi(num);
+        printf("%i",i);
+        //get the plain text
+        //use get_string to prompt the user for a plain text
+        string plain_text = get_string();
+
+        printf("%s",plain_text);
+
+
+        //encypher it for them
+        //one character
+        //entire plain text
+        //print cyphertext
+
+
+       
+        
+        //for each character in the plain text string
+        for(int i=0; i<strlen(plain_text); i++){
+            
+            if(isalpha(plain_text)){
+            
+                if(isupper(plain_text)){
+                
+                }
+                if(islower(plain_text)){
+                
+                }
+            
+            }
+            
+        }
         //if alphabetic
             //preserve case. All upper case letters should remain upper case
             //all lower case letters should remain lower case
             //shift plain text character by key
-    //print cypher text
+        //print cypher text
+
+
+
+    }
+
+}
