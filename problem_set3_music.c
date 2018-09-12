50c61bc86f19ade2ace62e60bb740beb3f23835a
mkirwa <mahlonkibiwott@gmail.com>
2018-09-11 21:29:34 -0500

    Add files via upload

--- problem_set3_music.c
+++ problem_set3_music.c
@@ -4,6 +4,8 @@
 
 int main(void){
 	//Each note on the piano is assigned a letter, A through G
-	//
+	//a semitone is the interval from one tone to the next
+	//When a black note is in between the white semitones then we say they are two semitones apart
+	//if the black note is absent then they are 1 semitone apart
 	
 }
