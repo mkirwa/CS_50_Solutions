f0a655d70f4b6ddcdb569c1a18abb5bfb7d2eb2e
mkirwa <mahlonkibiwott@gmail.com>
2018-09-11 21:37:31 -0500

    Add files via upload

--- problem_set3_music.c
+++ problem_set3_music.c
@@ -7,5 +7,14 @@ int main(void){
 	//a semitone is the interval from one tone to the next
 	//When a black note is in between the white semitones then we say they are two semitones apart
 	//if the black note is absent then they are 1 semitone apart
+	//so semitones are exactly how we are going to refer to a step of exactly
+	//one note on the piano keyboard
+	//So how are we going to represent the black keys on the piano key board? 
+	//white keys are all assigned letters
+	//So in order to represent the black keys, we will need to use accidentals
+	//accidentals adjust notes up or down one semitone
+	//so the sharp(#) accidental represented here by one pound sign means
+	//we are going to move up one semitone
+	//
 	
 }
