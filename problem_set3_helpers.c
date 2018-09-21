3e17bc0d609fd0703badc8f011c2e7ff111e2bb1
mkirwa <mahlonkibiwott@gmail.com>
2018-09-20 20:55:18 -0500

    Add files via upload

--- problem_set3_helpers.c
+++ problem_set3_helpers.c
@@ -27,6 +27,14 @@ int duration(string fraction)
 int frequency(string note)
 {
     // TODO
+    //this function should take as input as a string a note (e.g., A4)
+    //and return its corresponding frequency in hertz as an int.
+    //The frequency, f, of some note is 2n/12 × 440, 
+    //where n is the number of semitones from that note to A4.
+    //Each key on a piano is said to be one semitone, otherwise known as a half step,
+    //away from its adjacent neighbor, whether white or black.
+    //The effect of # and b, otherwise known as accidentals, is to raise or lower,
+    //respectively, the pitch of a note by one semitone.
 }
 
 // Determines whether a string represents a rest
