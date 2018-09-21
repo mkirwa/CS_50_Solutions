42e19d71d913afb0cd8384cdaf3ccd53a8f35301
mkirwa <mahlonkibiwott@gmail.com>
2018-09-20 20:50:25 -0500

    Add files via upload

--- problem_set3_helpers.c
+++ problem_set3_helpers.c
@@ -1,6 +1,5 @@
 // Helper functions for music
 
-
 #include <cs50.h>
 
 #include "helpers.h"
@@ -12,6 +11,10 @@
 int duration(string fraction)
 {
     // TODO
+    //this function should take as input as a string a fraction 
+    //and convert it into some integral number of eighths. You may
+    //assume that duration will only be passed a string formatted as X/Y,
+    //whereby each of X and Y is a positive decimal digit, and Y is, moreover, a power of 2.
 }
 
 // Calculates frequency (in Hz) of a note
