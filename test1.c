bf3fbe9b61b47efe43511622ef641609568a0b1b
mkirwa <mahlonkibiwott@gmail.com>
2018-10-01 10:27:55 -0500

    Add files via upload

--- test1.c
+++ test1.c
@@ -3,8 +3,9 @@
 #include "helpers.h"
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <math.h>
+
+
 
 // Converts a fraction formatted as X/Y to eighths
 //takes as input string a fraction (e.g., 1/4)
@@ -13,80 +14,31 @@
 int duration(string fraction);
 int main(void){
 
-     // TODO
-    //this function should take as input as a string a fraction
-    //and convert it into some integral number of eighths. You may
-    //assume that duration will only be passed a string formatted as X/Y,
-    //whereby each of X and Y is a positive decimal digit, and Y is, moreover, a power of 2.
+    string word = get_string("enter the word: ");
+    int converted = duration(word);
+    printf("%i",converted);
 
-    //X and Y should be positive decimal digits if not then return 1
-    //Y should be a power of 2 if not then return 1
-    //Input is a string  and it should be converted into some integral number of eights!
-        //what exactly does the user type in?
-        //I need to know if atoi gets rid of the / if not, how do I get rid of the /
+}
 
-
-        string word = get_string("enter the word: ");
-
-    //create a space for each character in the string
-    //store the characters in the spaces created
-        int num = strlen(word);
+int duration(string fraction)
+{
+        //which should take as input as a string a fraction (e.g., 1/4)
+        //return as an int a corresponding number of eigths
+        //(2, in this case, since 1/4 is equivalent to 2/8);
+        int num = strlen(fraction);
+        //checks to make sure X and Y is a positive decimal digits if not then return 1
         if(num>3){
             return 1;
         }
         else{
-
-                    if(word[2]%2!=0){
+                    //Y should be a power of 2 if not then return 1
+                    if(fraction[2]%2!=0){
                         return 1;
                     }else{
-
-                        double result1= atof(&word[2]);
+                        //converts the string into some integral number of eights
+                        double result1= atof(&fraction[2]);
                         result1 = trunc(log2(result1));
-
-                        return printf("%.0f%c%.0f\n",atof(&word[0])*result1,word[1],atof(&word[2])*result1);
-
-                    }
-                    //char array[num+1];
-
-                    //int count = 0;
-
-                    //for(int i= 0; i< num; i++){
-                        //array[count]=word[i];
-                        //count++;
-                    //}
-
-                    //array[count]='\0';
-
-
-                    //if that character is a number
-                    //if Y is a power of 2
-                        //convert it into some integral number of eighths
-                    //else return 1;
-
-                    //else store that character
-                    //print the stored number
-
-        }
-
-
+                        return atof(&fraction[0])*result1;
+                        }
+            }
 }
-
-//int duration(string fraction)
-//{
-    // TODO
-    //this function should take as input as a string a fraction
-    //and convert it into some integral number of eighths. You may
-    //assume that duration will only be passed a string formatted as X/Y,
-    //whereby each of X and Y is a positive decimal digit, and Y is, moreover, a power of 2.
-
-    //X and Y should be positive decimal digits if not then return 1
-    //Y should be a power of 2 if not then return 1
-    //Input is a string  and it should be converted into some integral number of eights!
-        //what exactly does the user type in?
-        //I need to know if atoi gets rid of the / if not, how do I get rid of the /
-
-    //return(atoi(fraction));
-
-
-
-//}
