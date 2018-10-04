21968f98c4f442bb50d3f10c1a1ae2dac3164bc1
mkirwa <mahlonkibiwott@gmail.com>
2018-10-04 13:44:50 -0500

    Add files via upload

--- test2.c
+++ test2.c
@@ -10,64 +10,44 @@ int duration(string fraction);
 int main(void){
 
     string word = get_string("enter the word: ");
-    int converted = note(word);
-    printf("%i",converted);
+    int converted = frequency(word);
+    printf("HERE%i",converted);
 
 }
 
 int frequency(string note)
 {
-        
-        int num = strlen(note);
-        if(num==2){
-            
-                    if((int)note[0]>=65 && (int)(note[0])<=71){
-                                
-                                //store note[0]
-                                
-                                //Return as an int the note’s corresponding frequency,
-                                //The frequency, f, of some note is 2n/12 × 440,
-                                //where n is the number of semitones from that note to A4
-                                
-                                
-                                
-                            //Y is any of 0 through 8, or Y is # or b
-                            }else if((int)note[1]>=0 && (int)note[1]<=8){
-                                
-                                
-                                
-                            }else{
-                                return 1;
-                            }
-            
-        }
-        else if(num==3){
-                    //take as input as a string a note formatted as XY (e.g., A4)
-                    //where X is any of A through G and Y is any of 0 through 8, or
-                    //XYZ (e.g., A#4), where X is any of A through G,
-                    //Y is # or b, and Z is any of 0 through 8,
-                    //rounded to the nearest integer;
-                    
-                    //where X is any of A through G 
-                    if((int)note[0]>=65 && (int)(note[0])<=71){
-                        
-                        //store note[0]
-                        //Return as an int the note’s corresponding frequency,
-                        //The frequency, f, of some note is 2n/12 × 440,
-                        //where n is the number of semitones from that note to A4
-                        
-                        
-                    //Y is any of 0 through 8, or Y is # or b
-                    }else if((int)note[1]==35 ||(int)note[1]==98){
-                        
-                        //store note
-                        
-                    }else if((int)note[1]>=0 && (int)note[1]<=8) {
-                        //store note
-                        
-                    }
-            }
-            else{
+
+    int num = strlen(note);
+    if(num==2){
+
+            if((int)note[0]>=65 && (int)(note[0])<=71){
+                int num1=atoi(&note[1]);
+                if(num1>=0 && num1<=8){
+                    return printf("it's working%i%i",note[0],num1);
+                }else{
+                    return 1;
+                }
+
+            }else{
                 return 1;
             }
+
+    }else if(num==3){
+            if((int)note[0]>=65 && (int)(note[0])<=71){
+                return printf("it's working%i",note[0]);
+            }else if((int)note[1]==35 ||(int)note[1]==98){
+                return printf("it's working%i",note[0]);
+            }else if((int)note[1]>=0 && (int)note[1]<=8){
+                return printf("it's working%i",note[0]);
+            }else{
+                return 1;
+            }
+
+    }else{
+        return 1;
+    }
+
+
 }
+
