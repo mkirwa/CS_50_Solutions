2289fb0ccf7cb522f21eda78fb30ec330203742f
mkirwa <mahlonkibiwott@gmail.com>
2018-10-03 21:24:32 -0500

    Add files via upload

--- test2.c
+++ test2.c
@@ -1,26 +1,51 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+#include "helpers.h"
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
+
+// Converts a fraction formatted as X/Y to eighths
+//takes as input string a fraction (e.g., 1/4)
+//and return as an int a corresponding number of eigths
+//(2, in this case, since 1/4 is equivalent to 2/8)
+int duration(string fraction);
 int main(void){
 
-
-	String
+    string word = get_string("enter the word: ");
+    int converted = note(word);
+    printf("%i",converted);
 
 }
 
-
 int frequency(string note)
 {
-    // TODO
-    //this function should take as input as a string a note (e.g., A4)
-    //and return its corresponding frequency in hertz as an int.
-    //The frequency, f, of some note is 2n/12 × 440,
-    //where n is the number of semitones from that note to A4.
-    //Each key on a piano is said to be one semitone, otherwise known as a half step,
-    //away from its adjacent neighbor, whether white or black.
-    //The effect of # and b, otherwise known as accidentals, is to raise or lower,
-    //respectively, the pitch of a note by one semitone.
-
+        //which should take as input as a string a fraction (e.g., 1/4)
+        //return as an int a corresponding number of eigths
+        //(2, in this case, since 1/4 is equivalent to 2/8);
+        int num = strlen(note);
+        //checks to make sure X and Y is a positive decimal digits if not then return 1
+        if(num>3){
+            return 1;
+        }
+        else{
+                    //take as input as a string a note formatted as XY (e.g., A4)
+                    //where X is any of A through G and Y is any of 0 through 8, or
+                    //XYZ (e.g., A#4), where X is any of A through G,
+                    //Y is # or b, and Z is any of 0 through 8,
+                    //and return as an int the note’s corresponding frequency,
+                    //rounded to the nearest integer;
+                    
+                    //where X is any of A through G 
+                    if(int(note[0])>=65 && int(note[0])<=71){
+                        return 1;
+                    }else{
+                        //converts the string into some integral number of eights
+                        double result1= atof(&fraction[2]);
+                        result1 = trunc(log2(result1));
+                        return atof(&fraction[0])*result1;
+                        }
+            }
 }
