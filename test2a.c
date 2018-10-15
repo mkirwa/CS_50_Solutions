94bd21c24792bbb1a1eda69fe7e47092950c9c19
mkirwa <mahlonkibiwott@gmail.com>
2018-10-14 19:04:27 -0500

    Add files via upload

--- test2a.c
+++ test2a.c
@@ -31,14 +31,14 @@ int main(void){
 
 int frequency(string note)
 {
-
     if(strlen(note[0])=2 && num<=3){
-            
-            const char array = {'A','A','B','C','C','D','D','E','F','F','G','G'};
+
+        //loop through the length of the array until the input is equivalent to
+        //one of the characters in the array    
+            const char array[] = {'A','A','B','C','C','D','D','E','F','F','G','G'};
             
             for(int i = 0; i<=diff; i++){
                 
-                    
                 
                     difference = count-count2;
                     power = pow(2,(difference/12));
